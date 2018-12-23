4625e1ec6831fc5d4983565f6ea911d1869a4294
bribrah <cheesemas46@gmail.com>
2018-12-23 13:23:46 -0800

    added a projects static page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+  get "/projects", to: "static_pages#projects"
   resources :posts
 end

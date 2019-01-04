179618e2a7ea858937f69f79cbdf0e330a2a04f9
bribrah <cheesemas46@gmail.com>
2019-01-04 14:32:06 -0800

    finished adding enhanced image

---
+++ app/assets/javascripts/ckeditor/config.js
@@ -0,0 +1,5 @@
+
+CKEDITOR.editorConfig = function( config )
+{
+  config.extraPlugins = 'image2','dialog','widget',;
+}
--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -46,6 +46,14 @@ Ckeditor.setup do |config|
   # config.assets_languages = ['en', 'uk']
   # config.assets_plugins = ['image', 'smiley']
 
+  #handle custom addons
+assets_root =  Rails.root.join('app','assets','javascripts')
+ckeditor_plugins_root = assets_root.join('ckeditor','plugins')
+%w(openlink sourcedialog).each do |ckeditor_plugin|
+  Ckeditor.assets += Dir[ckeditor_plugins_root.join(ckeditor_plugin, '**', '*.js')].map do |x|
+    x.sub(assets_root.to_path, '').sub(/^\/+/, '')
+  end
+end
   # CKEditor CDN
   # More info here http://cdn.ckeditor.com/
   # By default: nil (CDN disabled)

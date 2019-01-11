4bda7b51c877e3d85e9b523731ac54f5a4040bac
bribrah <cheesemas46@gmail.com>
2019-01-11 15:11:16 -0800

    GOT ENHANCED IMAGE AND A COUPLE OTHER CKEDITOR PLUGINS TO WORK!

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,6 +1,6 @@
 CKEDITOR.editorConfig = function( config )
 {
-  config.extraPlugins = 'dialog,dialogui,eqneditor';
+  config.extraPlugins = 'youtube,eqneditor,image2,lineutils,widget,dialog,dialogui';
    // The location of an external file browser, that should be launched when "Browse Server" button is pressed in the Link tab of Image dialog.
    config.filebrowserImageBrowseLinkUrl = "/ckeditor/pictures";
 

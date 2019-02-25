6973cfb5d46814fe373eb8302965d1cbdae056b4
bribrah <cheesemas46@gmail.com>
2019-02-25 00:28:32 -0800

    added comment error messages and validations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,17 @@ class CommentsController < ApplicationController
     def create
         @comment = Comment.new(comment_params)
         @comment.post_id = params[:post_id]
-
-        @comment.save
-
-        redirect_to post_path(@comment.post)
+        if @comment.save
+            flash[:success] = "Comment created successfully"
+            redirect_to post_path(@comment.post)
+        else
+            flash.now[:warning] = "comment not created successfully"
+            params[:id] = @comment.post_id
+            render "posts/show"
+        end
     end
 
+    private
     def comment_params
         params.require(:comment).permit(:author_name, :body)
     end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :post
+
+    validates :author_name, presence: true, length: {maximum: 50}
+    validates :body, presence: true, length: {maximum: 250}
 end

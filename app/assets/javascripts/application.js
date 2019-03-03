6dbee14219941a5ad96655c6293c3311b56c0fb2
bribrah <cheesemas46@gmail.com>
2019-03-03 01:49:49 -0800

    fuck u

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
 //= require ckeditor/init
 //= require rails-ujs
 //= require activestorage
+//= require turbolinks
 //= require_tree .

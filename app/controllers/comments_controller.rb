06dedf1d275e17733282042b8d82c76bb79714dc
bribrah <cheesemas46@gmail.com>
2019-03-14 12:32:07 -0700

    got comment deletion working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  http_basic_authenticate_with name: "bribrah", password: "people46", only: [:destroy]
+    
     def create
         @comment = Comment.new(comment_params)
         @comment.post_id = params[:post_id]
@@ -11,6 +13,19 @@ class CommentsController < ApplicationController
             render "posts/show"
         end
     end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        post_id = params[:post_id]
+        if @comment.destroy
+            flash[:success] = 'Comment was successfully deleted.'
+            redirect_to post_path(post_id)
+        else
+            flash[:error] = 'Something went wrong'
+            redirect_to comments_url
+        end
+    end
+    
 
     private
     def comment_params

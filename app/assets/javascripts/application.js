e0a7de0ce678e5efb73c256ee61b3d06b67cc8c8
bribrah <cheesemas46@gmail.com>
2019-03-24 17:01:19 -0700

    disabled turblonks temporarily because broke JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
+// require turbolinks
 //= require ckeditor/init
 //= require jquery
 //= require bootstrap
